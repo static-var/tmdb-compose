4532ee7f35d1cf9083078581315061327fb966c9
“UnusedVariable” <slodha96@gmail.com>
2020-11-21 23:04:58 +0530

    Update appbar with icon
    Signed-off-by: “UnusedVariable” <slodha96@gmail.com>

--- app/src/main/java/dev/shreyansh/tmdb/ui/Components.kt
+++ app/src/main/java/dev/shreyansh/tmdb/ui/Components.kt
@@ -1,31 +1,37 @@
 package dev.shreyansh.tmdb.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.imageFromResource
-import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import dev.shreyansh.tmdb.R
 
 @Composable
 fun TmDBAppBar(showBack: Boolean, backAction: () -> Unit = {}) {
-    TopAppBar(
-        title = {
-            Text(text = "tmDB", modifier = if (showBack) Modifier else Modifier.fillMaxWidth(), textAlign = if (showBack) null else TextAlign.Center)
-        },
-        backgroundColor = MaterialTheme.colors.background,
-        navigationIcon = if (showBack) {
-            {
+    Row(modifier = Modifier.height(56.dp).fillMaxWidth()) {
+        Surface(color = Color.Transparent) {
+            if (showBack) {
                 IconButton(onClick = backAction) {
-                    Icon(vectorResource(R.drawable.ic_baseline_arrow_back))
+                    Icon(
+                        asset = vectorResource(id = R.drawable.ic_baseline_arrow_back),
+                        modifier = Modifier.padding(4.dp)
+                    )
                 }
+            } else {
+                Image(
+                    asset = vectorResource(id = R.drawable.ic_tmdb_logo),
+                    modifier = Modifier.padding(8.dp)
+                )
             }
-        } else null,
-        elevation = 0.dp
-    )
+        }
+    }
 }
