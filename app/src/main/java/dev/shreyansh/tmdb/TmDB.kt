23a373f1d75e79ba7158f0c59a04524a7dc13495
“UnusedVariable” <slodha96@gmail.com>
2020-11-22 19:41:32 +0530

    Initialize Timber tree for debug builds
    Signed-off-by: “UnusedVariable” <slodha96@gmail.com>

--- app/src/main/java/dev/shreyansh/tmdb/TmDB.kt
+++ app/src/main/java/dev/shreyansh/tmdb/TmDB.kt
@@ -2,7 +2,21 @@ package dev.shreyansh.tmdb
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class TmDB : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG)
+            Timber.plant(object : Timber.DebugTree() {
+                override fun createStackElementTag(element: StackTraceElement): String? {
+                    with(element) {
+                        return "($fileName:$lineNumber)$methodName()"
+                    }
+                }
+            })
+    }
 }
