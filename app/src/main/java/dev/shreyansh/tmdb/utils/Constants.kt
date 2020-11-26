51fdbbd6bc3b1d24b2feed379c5c7501bb6902d0
“UnusedVariable” <slodha96@gmail.com>
2020-11-26 23:29:44 +0530

    Add URL to constants class
    Signed-off-by: “UnusedVariable” <slodha96@gmail.com>

--- app/src/main/java/dev/shreyansh/tmdb/utils/Constants.kt
+++ app/src/main/java/dev/shreyansh/tmdb/utils/Constants.kt
@@ -1,9 +1,16 @@
 package dev.shreyansh.tmdb.utils
 
 object Constants {
+    object URL {
+        const val BASE_URL = "https://api.themoviedb.org/"
+        const val POSTER_URL = "https://image.tmdb.org/t/p/w780"
+        const val BACKDROP_URL = "https://image.tmdb.org/t/p/w500"
+    }
     object Database {
         const val DB_NAME = "tmdb"
         const val GENRE_TABLE = "genre"
+        const val MOVIE_TABLE = "movie"
+        const val TV_SHOW_TABLE = "tv_show"
     }
 
 }
