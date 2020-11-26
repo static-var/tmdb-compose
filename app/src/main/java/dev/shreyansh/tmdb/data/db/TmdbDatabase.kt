bed26533ddee4268a10c183d0eabceb7d31d8dbb
“UnusedVariable” <slodha96@gmail.com>
2020-11-26 23:42:31 +0530

    Use new function for injecting moshi component
    Signed-off-by: “UnusedVariable” <slodha96@gmail.com>

--- app/src/main/java/dev/shreyansh/tmdb/data/db/TmdbDatabase.kt
+++ app/src/main/java/dev/shreyansh/tmdb/data/db/TmdbDatabase.kt
@@ -2,18 +2,28 @@ package dev.shreyansh.tmdb.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import dev.shreyansh.tmdb.data.db.dao.GenreDao
+import dev.shreyansh.tmdb.data.db.dao.MovieDao
+import dev.shreyansh.tmdb.data.db.dao.TvShowDao
 import dev.shreyansh.tmdb.data.model.Genre
+import dev.shreyansh.tmdb.data.model.Movie
+import dev.shreyansh.tmdb.data.model.TvShow
 
 @Database(
     entities = [
-        Genre::class
+        Genre::class,
+        Movie::class,
+        TvShow::class,
     ],
     version = 1,
     exportSchema = false
 )
+@TypeConverters(TmdbTypeConverters::class)
 abstract class TmdbDatabase: RoomDatabase() {
 
     abstract fun genreDao(): GenreDao
+    abstract fun movieDao(): MovieDao
+    abstract fun tvShowDao(): TvShowDao
 
 }
