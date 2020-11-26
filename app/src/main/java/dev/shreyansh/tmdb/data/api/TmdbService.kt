668bdc4cc87a88cba4c572810adb099745b8c36f
“UnusedVariable” <slodha96@gmail.com>
2020-11-26 23:40:53 +0530

    Add Api calls for trending movies and tvshows
    Signed-off-by: “UnusedVariable” <slodha96@gmail.com>

--- app/src/main/java/dev/shreyansh/tmdb/data/api/TmdbService.kt
+++ app/src/main/java/dev/shreyansh/tmdb/data/api/TmdbService.kt
@@ -2,10 +2,21 @@ package dev.shreyansh.tmdb.data.api
 
 import com.skydoves.sandwich.ApiResponse
 import dev.shreyansh.tmdb.data.model.responses.GenreListResponse
+import dev.shreyansh.tmdb.data.model.responses.MovieListResponse
+import dev.shreyansh.tmdb.data.model.responses.TvShowListResponse
 import retrofit2.http.GET
 
 interface TmdbService {
 
     @GET("/3/genre/movie/list")
-    suspend fun getListOfGenre(): ApiResponse<GenreListResponse>
+    suspend fun getListOfMovieGenre(): ApiResponse<GenreListResponse>
+
+    @GET("/3/genre/tv/list")
+    suspend fun getListOfTvShowGenre(): ApiResponse<GenreListResponse>
+
+    @GET("/3/trending/movie/week")
+    suspend fun getTrendingMovies(): ApiResponse<MovieListResponse>
+
+    @GET("/3/trending/tv/week")
+    suspend fun getTrendingShows(): ApiResponse<TvShowListResponse>
 }
